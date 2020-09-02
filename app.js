3347b120755a806a3d5f36157d9fbb926dd2c705
Alessio Burgio <alessioburgio85@gmail.com>
2020-09-02 22:27:24 +0200

    port

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use((req, res, next) => {
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 
-const PORT = process.env.PORt || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on ${PORT}`));
 
